f3bf07585bf42b335ab38d0541beb22fe8f290dc
gabrieldeluca88 <gabrieldeluca88@gmail.com>
2023-01-26 03:27:27 -0300

    railway

---
+++ railway/src/controller/productosMongo.js
@@ -0,0 +1,63 @@
+const {ProductsModel} = require ("../schemas/productos.js")
+
+class ClientMongo {
+    async getAllProducts() {
+        const productos = await ProductsModel.find();
+        return productos
+    }
+
+    async getProductById(id) {
+        const product = await ProductsModel.findById(id);
+        if(!product){
+            return "Producto no encontrado :c"
+        }
+
+        return product
+    }
+
+    async createProduct (object){
+        const {title, price,thumbnail,timestamp,descripcion,codigo,stock} = object
+        const newProduct = await ProductsModel.create({
+            title,
+            price,
+            thumbnail,
+            timestamp,
+            descripcion,
+            codigo,
+            stock
+        })
+
+        return newProduct
+    }
+
+    async updateProduct (id, array){
+        const {title, price,thumbnail,timestamp,descripcion,codigo,stock} = array
+        const product = await ProductsModel.findById(id);
+        if(!product){
+            return "Producto no encontrado :("
+        }
+        const productUpdated = await ProductsModel.findByIdAndUpdate(
+            id,
+            { title, price,thumbnail,timestamp,descripcion,codigo,stock },
+            { new: true }
+        );
+
+        return productUpdated
+    }
+
+    async deleteProduct (id){
+        const product = await ProductsModel.findByIdAndDelete(id)
+        const data = {
+            msg: "Producto eliminado",
+            product
+        }
+
+        return data
+    }
+}
+
+const MongoProductosController = new ClientMongo();
+
+module.exports = {
+    MongoProductosController
+}
---
+++ railway/src/routes/usuarios.js
@@ -0,0 +1,22 @@
+const { Router } = require ("express")
+const passport = require ("passport");
+const { signUp, login, getHome } = require ('../controller/userController.js');
+const info = require ("../middlewares/logger.js")
+
+const rutaUsuarios = Router();
+
+const passportOptions = { badRequestMessage: "Falta email / password"}
+
+const isLoggedIn = (req, res, next) => {
+    console.log(req.isAuthenticated());
+    if(!req.isAuthenticated()) return res.status(401).json({msg: 'Unauthorized'});
+    next();
+}
+
+rutaUsuarios.post('/signup', info, signUp)
+
+rutaUsuarios.post('/login',info, passport.authenticate('login', passportOptions), login);
+
+rutaUsuarios.get('/home',info, isLoggedIn, getHome )
+
+module.exports = rutaUsuarios;
